b69b45e6ad2719fcd0cb1d37cf9487d7b82885c2
NdumisoMagunga <magunga55@gmail.com>
2023-08-21 06:19:01 +0200

    added the rolldice file and initialized the first tow die states

--- src/Die.js
+++ src/Die.js
@@ -4,9 +4,9 @@ import "./Die.css";
 class Die extends Component {
     render(){
         return(
-            <div>
+            
                 <i className={`die fas fa-dice-${this.props.face}`} />
-            </div>
+           
         )
     }
 }
